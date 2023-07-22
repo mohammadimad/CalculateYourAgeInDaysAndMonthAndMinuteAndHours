6ada3ad1fd4a132ace56a835e34067b658903813
mohammad abdelfattah <hammodaimad@gmail.com>
2023-07-22 08:01:59 +0300

    this program calcute your age in detals

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace Calculateyourdateofbirth
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDate = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePickerBirthdate = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtDate
+            // 
+            this.txtDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDate.Location = new System.Drawing.Point(230, 94);
+            this.txtDate.Name = "txtDate";
+            this.txtDate.Size = new System.Drawing.Size(182, 26);
+            this.txtDate.TabIndex = 2;
+            this.txtDate.Tag = "";
+            this.txtDate.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(230, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 76);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "احسب عمرك";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePickerBirthdate
+            // 
+            this.dateTimePickerBirthdate.Location = new System.Drawing.Point(230, 34);
+            this.dateTimePickerBirthdate.MinDate = new System.DateTime(1900, 1, 1, 0, 0, 0, 0);
+            this.dateTimePickerBirthdate.Name = "dateTimePickerBirthdate";
+            this.dateTimePickerBirthdate.Size = new System.Drawing.Size(182, 20);
+            this.dateTimePickerBirthdate.TabIndex = 0;
+            this.dateTimePickerBirthdate.ValueChanged += new System.EventHandler(this.dateTimePickerBirthdate_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(476, 34);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(164, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ادخل تاريخ ميلادك :  ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(86)))), ((int)(((byte)(165)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(738, 493);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePickerBirthdate);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDate);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDate;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePickerBirthdate;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculateyourdateofbirth
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public struct stDate
+        {
+            public int Day;
+            public int Month;
+            public int Year;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DateTime Date = new DateTime();
+            
+            dateTimePickerBirthdate.MaxDate = DateTime.Today;
+           
+        }
+        public stDate StringToDate(string DateString)
+        {
+            stDate Date = new stDate();
+            string[] vDate;
+
+            vDate = DateString.Split('/');
+            Date.Day = int.Parse(vDate[0]);
+            Date.Month = int.Parse(vDate[1]);
+            Date.Year = int.Parse(vDate[2]);
+            return Date;
+        
+    }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime birthdate = dateTimePickerBirthdate.Value;
+            DateTime today = DateTime.Today;
+            int ageInDays = (today - birthdate).Days;
+            int ageInYear;
+            int ageInMonth;
+            int NumberInMonth;
+            NumberInMonth = DateTime.DaysInMonth(birthdate.Year, birthdate.Month);
+            if (DateTime.IsLeapYear(birthdate.Year))
+            {              
+                ageInYear = ageInDays / 366;
+            }
+            else
+            {
+                ageInYear = ageInDays / 365;
+            }
+            ageInMonth = ageInDays / NumberInMonth;
+            int ageInHours = ageInDays * 24;
+            int ageInMinute = ageInHours * 60;
+            int ageInSecond = ageInMinute * 60;
+            MessageBox.Show("عمرك بالسنوات : " + ageInYear.ToString() + "\n عمرك بالاشهر : " + ageInMonth.ToString()
+                +"\n" + "عمرك بالايام : " + ageInDays.ToString() + "\n" + "عمرك بالساعات : " + ageInHours.ToString()
+                + "\n" + "عمرك الدقائق : " + ageInMinute.ToString() + "\n" + "عمرك بالثواني : " + ageInSecond.ToString(), "Detals", MessageBoxButtons.OK) ;
+
+
+         
+           
+        }
+
+        private void dateTimePickerBirthdate_ValueChanged(object sender, EventArgs e)
+        {
+            txtDate.Text = dateTimePickerBirthdate.Value.ToString();
+        }
+    }
+}
